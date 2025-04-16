67a893f5da2eff7a2ec5e3a21ce8d06305b4d4df
Mahdioui Salma <salmamahdioui8@gmail.com>
2025-04-16 10:32:23 +0100

    commit de la 3ème branche

--- devops.py
+++ devops.py
@@ -1,3 +1,3 @@
 def hello():
     print("hello developper, I am the scrum master!")
-    
+    print("Je suis la 3ème branche de ve projet. Mon objectif est de comprendre la différence entre clone/fork et merge/rebase")
